ca4f7c3442cd5737e27e312f75b5efb626ffd824
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2020-05-26 11:37:48 -0700

    clean out dummy data

--- config.js
+++ config.js
@@ -2,10 +2,9 @@ const config = {
   verbose: false,
   port: 1337,
   webhook: {
-    url:
-      "https://discordapp.com/api/webhooks/714901741761724574/Vc08vJygbPPu2zKOL_w55MUVc7AdddUlXlTnci8IfvYFEF-YNzQQ9rOt667LleqzS5Pp",
-    username: "wss://relay.amnesia.software",
-    avatar: "https://i.imgur.com/6DK7Yrc.jpg",
+    url: "",
+    username: "",
+    avatar: "",
   },
 };
 
