52f09d91ca309b1855967cdc2bec3e18762a1621
alectrocute <35377827+alectrocute@users.noreply.github.com>
2020-05-26 11:34:17 -0700

    adds config, bug fixes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const VERBOSE = false;
 let PORT;
 
+const config = require("./config.js").config;
+
+const VERBOSE = config.verbose;
+
 const defaults = {
-  port: 8080,
+  port: process.argv[2] ? process.argv[2] : config.port,
+  webhookURL: config.webhook.url ? config.webhook.url : "",
 };
+
+if (VERBOSE) console.info(JSON.stringify(defaults, null, 2));
 
 const strings = {
   failedLogin:
@@ -11,21 +17,47 @@ const strings = {
   infoString: "zen-relay v." + require("./package.json").version,
   listening: "zen-relay is now listening on http://localhost:<PORT>",
   alreadyInUse: "Exited. Requested listening address is already in use.",
-  noPort: "No port specified, defaulting to http://localhost:8080...",
+  noPort: `No port specified, defaulting to http://localhost:${defaults.port}...`,
+  webhook: {
+    username: config.webhook.username,
+    avatar: config.webhook.avatar,
+  },
 };
 
 const app = require("express")();
 const http = require("http").createServer(app);
+const axios = require("axios");
 const io = require("socket.io")(http, { pingInterval: 2000 });
 const cors = require("cors");
 
-const logger = function (l) {
-  if (!VERBOSE) return;
-  console.log(l);
+const webhook = function (l) {
+  const params = {
+    username: strings.webhook.username,
+    avatar_url: strings.webhook.avatar,
+    content: l,
+  };
+
+  axios
+    .post(defaults.webhookURL, params)
+    .then((res) => {})
+    .catch((e) => {
+      if (VERBOSE) console.log(e);
+    });
 };
 
-if (!process.argv[2]) console.error(strings.noPort);
-PORT = process.argv[2] ? process.argv[2] : defaults.port;
+const logger = function (l) {
+  if (defaults.webhookURL) {
+    webhook(l);
+  }
+  if (VERBOSE) console.log(l);
+};
+
+if (!process.argv[2] && !process.env.PORT) console.error(strings.noPort);
+PORT = process.argv[2]
+  ? process.argv[2]
+  : process.env.PORT
+  ? process.env.PORT
+  : defaults.port;
 
 // keep track of connected clients
 let guests = [];
@@ -82,13 +114,14 @@ io.sockets.on("connection", function (socket) {
 });
 
 app.get("/stats", function (req, res) {
+  webhook("test");
   res.json({
     connected_clients: guests.length,
     health: "ok",
   });
 });
 
-app.use(function (req, res, next) {
+app.use(function (req, res) {
   res.status(404).send(strings.infoString);
 });
 
